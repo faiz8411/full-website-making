a7f07ccd85f4ae722216f833564caf500b76370a
faiz8411 <faizullah.noa2014@gmail.com>
2022-03-12 00:25:11 +0600

    swal

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,28 +1,25 @@
-import React from 'react';
-import Footer from '../../Shared/Footer/Footer';
-import Navigation from '../../Shared/Navigation/Navigation';
-import AllReview from '../AllReview/AllReview';
-import Banner from '../Banner/Banner';
-import Services from '../Services/Services';
-import Text from '../Text/Text';
-import About from './About/About';
+import React from "react";
+import Footer from "../../Shared/Footer/Footer";
+import Navigation from "../../Shared/Navigation/Navigation";
+import AllReview from "../AllReview/AllReview";
+import Services from "../Services/Services";
+import Carusel from "../Carusel/SwipeableTextMobileStepper";
+import Text from "../Text/Text";
+import About from "./About/About";
 
 const Home = () => {
-    return (
-        <div>
-            <Navigation></Navigation>
+  return (
+    <div>
+      <Navigation></Navigation>
+      <Carusel></Carusel>
+      <AllReview></AllReview>
+      <Services></Services>
 
-            <Banner></Banner>
-            <AllReview></AllReview>
-            <Services></Services>
+      <About></About>
 
-            <About></About>
-
-
-
-            <Footer></Footer>
-        </div>
-    );
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Home;
